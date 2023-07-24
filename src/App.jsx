fa163555af18ec6a6c0ebc06e22374db76835f2c
isaacbaldridge <baldridgela@alumni.vcu>
2023-07-24 19:39:32 -0400

    display featured puppy info

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,29 @@ import './App.css'
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList)
-  console.log("puppyList", puppies)
+  const [featPupId, setFeatPupId] = useState(null)
+  // console.log("puppyList", puppies)
+  const featuredPup = puppies.find((pup)=>
+  pup.id === featPupId)
+
+  console.log(featuredPup)  
+  
   return (
-      <div className = "App">
+    <div className = "App">
         {
           puppies.map((puppy) => {
-            return <p key = {puppy.id}>{puppy.name}</p>
+            return <p onClick={()=>{ setFeatPupId(puppy.id)}} key = {puppy.id}>{puppy.name}</p>
           })
         }
+        {featPupId && (
+          <div>
+            <h2>{featuredPup.name}</h2>
+            <ul>
+              <li>Age: {featuredPup.name}</li>
+              <li>Email: {featuredPup.email}</li>
+            </ul>
+          </div>
+        )}
       </div>
   )
 }
